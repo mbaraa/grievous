993d3310bd8d83b02435a668c37a0ca15a7b77cc
Baraa Al-Masri <m.baraa.masri@gmail.com>
2023-02-24 19:37:09 +0000

    added save to wave file option

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -7,12 +7,14 @@ import (
 
 	"github.com/mbaraa/grievous/freqsjuicer"
 	"github.com/mbaraa/grievous/player"
+	"github.com/mbaraa/grievous/savefile"
 )
 
 var (
 	flags = flag.NewFlagSet("Grievous", flag.ExitOnError)
 	url   = new(string)
 	file  = new(string)
+	wav   = new(string)
 )
 
 func Start() {
@@ -21,27 +23,38 @@ func Start() {
 	runWithGivenArgs()
 }
 
+func quitIfError(err error) {
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+}
+
 func runWithGivenArgs() {
+	var freqs []int
+	var err error
 	if len(*url) > 0 {
-		freqs, err := freqsjuicer.JuiceURL(*url)
-		if err != nil {
-			_, _ = fmt.Fprintln(os.Stderr, err.Error())
-			return
-		}
-		player.PlaySound(freqs)
+		freqs, err = freqsjuicer.JuiceURL(*url)
+		quitIfError(err)
 	} else if len(*file) > 0 {
-		freqs, err := freqsjuicer.JuiceFile(*file)
-		if err != nil {
-			_, _ = fmt.Fprintln(os.Stderr, err.Error())
-			return
-		}
-		player.PlaySound(freqs)
+		freqs, err = freqsjuicer.JuiceFile(*file)
+		quitIfError(err)
 	} else {
 		flags.Usage()
+		return
+	}
+
+	if len(*wav) != 0 {
+		out, err := os.Create(*wav)
+		quitIfError(err)
+		savefile.WriteWaveFile(out, freqs)
+	} else {
+		player.PlaySound(freqs)
 	}
 }
 
 func registerFlags() {
 	flags.StringVar(url, "url", "", "generate noises from a specific URL")
 	flags.StringVar(file, "file", "", "generate noises from a specific file")
+	flags.StringVar(wav, "wav", "", "save noises to the specified wav file")
 }
