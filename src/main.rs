f5d16fe48b974bfb322988f385ef2f93d2dc51af
Baraa Al-Masri <pub@mbaraa.com>
2024-09-22 15:07:48 +0300

    Add rest of the running stuff

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use audio_player::alsa;
-use audio_player::player::{Note, Player};
-use futures::executor;
+use audio_player::alsa::{self, AlsaPlayer};
+use audio_player::player::{AudioError, Note, Player};
 use regex::Regex;
 use reqwest;
-use std::env;
 use std::error::Error;
 use std::fmt::Display;
+use std::{env, fs};
 
 mod audio_player;
 
@@ -14,6 +13,9 @@ enum AppError {
     ShortArgs,
     InvalidArgs,
     Http,
+    FileNotFound,
+    Juicing,
+    Audio(AudioError),
 }
 
 impl Error for AppError {}
@@ -103,29 +105,56 @@ fn get_run_mode_from_args() -> Result<RunMode, AppError> {
 async fn main() -> Result<(), AppError> {
     match get_run_mode_from_args() {
         Ok(rm) => match rm {
-            RunMode::Play(st) => {
-                match st {
-                    SourceType::Url(url) => println!("{}", url),
-                    SourceType::File(path) => println!("{}", path),
-                    SourceType::Invalid => {}
-                }
-                todo!("impl play")
-            }
-            RunMode::Wav(st) => {
-                match st {
-                    SourceType::Url(url) => println!("{}", url),
-                    SourceType::File(path) => println!("{}", path),
-                    SourceType::Invalid => {}
-                }
-                todo!("impl wav")
-            }
+            RunMode::Play(st) => play_from_source(st).await,
+            RunMode::Wav(st) => play_from_source(st).await,
             RunMode::Invalid => Err(AppError::InvalidArgs),
         },
         Err(err) => Err(err),
     }
 }
 
-async fn juice_url(url: &str) -> Result<Vec<u16>, AppError> {
+async fn play_from_source(src: SourceType) -> Result<(), AppError> {
+    let freqs = match src {
+        SourceType::Url(url) => juice_url(url.as_str()).await,
+        SourceType::File(path) => juice_file(path.as_str()),
+        SourceType::Invalid => Err(AppError::InvalidArgs),
+    };
+    if freqs.is_err() {
+        return Err(AppError::Juicing);
+    }
+
+    let bitrate = 44100;
+    let note_duration = 0.9f32;
+    let player = AlsaPlayer::new(bitrate);
+
+    let result = player.play_sound(
+        freqs
+            .unwrap()
+            .iter()
+            .map(|f| Note::new(*f as f32, note_duration))
+            .collect(),
+    );
+    if result.is_err() {
+        return Err(AppError::Audio(result.err().unwrap()));
+    }
+
+    Ok(())
+}
+
+fn juice_file(path: &str) -> Result<Vec<u32>, AppError> {
+    let file_content = fs::read_to_string(path);
+    if file_content.is_err() {
+        return Err(AppError::FileNotFound);
+    }
+    Ok(file_content
+        .unwrap()
+        .replace("\n", "")
+        .bytes()
+        .map(|f| f as u32)
+        .collect())
+}
+
+async fn juice_url(url: &str) -> Result<Vec<u32>, AppError> {
     let ml_pat = Regex::new(r"<[^>]*>").unwrap();
     let resp = reqwest::get(url).await;
     if resp.is_err() {
@@ -138,11 +167,11 @@ async fn juice_url(url: &str) -> Result<Vec<u16>, AppError> {
     let resp_body_unwrapped = resp_body.unwrap();
     let replaced_body = ml_pat.replace_all(resp_body_unwrapped.as_str(), "");
 
-    Ok::<Vec<u16>, AppError>(
+    Ok::<Vec<u32>, AppError>(
         replaced_body
             .replace("\n", "")
             .bytes()
-            .map(|f| f as u16)
+            .map(|f| f as u32)
             .collect(),
     )
 }
